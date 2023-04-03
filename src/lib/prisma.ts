62dac06675dc533be054ad6466eb7020f63c976f
Lance Armah-Abraham <lanarmah@gmail.com>
2023-04-03 22:22:26 +0000

    feat: use edge for prisma connections

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,19 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
 
 declare global {
-  var prisma: PrismaClient | undefined;
+	var prisma: PrismaClient | undefined;
 }
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+	prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient({ log: ['query'] });
-  }
-  prisma = global.prisma;
+	if (!global.prisma) {
+		global.prisma = new PrismaClient({ log: ['query'] });
+	}
+	prisma = global.prisma;
 }
 
 export default prisma;
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,9 @@
 import type { PageServerLoad } from './$types';
+import type { Config } from '@sveltejs/adapter-vercel';
+
+export const config: Config = {
+	runtime: 'edge'
+};
 
 import prisma from '$lib/prisma';
 
