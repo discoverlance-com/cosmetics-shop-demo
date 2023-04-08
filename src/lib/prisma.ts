de4838c69128a8a2af840d4405d935c58e3c9279
Lance Armah-Abraham <lanarmah@gmail.com>
2023-04-08 10:59:30 +0000

    feat: use edge client only

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,17 @@
 // lib/prisma.ts
 import { PrismaClient as PrismaEdgeClient } from '@prisma/client/edge';
-import { PrismaClient } from '@prisma/client';
 
 declare global {
-	var prisma: PrismaClient | undefined;
+	var prisma: PrismaEdgeClient | undefined;
 }
 
-let prisma: PrismaClient;
+let prisma: PrismaEdgeClient;
 
 if (process.env.NODE_ENV === 'production') {
 	prisma = new PrismaEdgeClient();
 } else {
 	if (!global.prisma) {
-		global.prisma = new PrismaClient({ log: ['query'] });
+		global.prisma = new PrismaEdgeClient({ log: ['query'] });
 	}
 	prisma = global.prisma;
 }
