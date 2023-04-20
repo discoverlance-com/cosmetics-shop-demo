49981ca55a46f4ebcd743f6edcb5ab15134baba7
Lance Armah-Abraham <lanarmah@gmail.com>
2023-04-20 18:25:26 +0000

    feat: add vercel speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,20 @@
 	import GlobalNavigationSpinner from '$components/UI/spinners/GlobalNavigationSpinner.svelte';
 	import '../app.css';
 
+	import { webVitals } from '$lib/vitals';
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
+
 	inject();
+
+	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+	$: if (browser && analyticsId) {
+		webVitals({
+			path: $page.url.pathname,
+			params: $page.params,
+			analyticsId
+		});
+	}
 </script>
 
 <div class="relative">
