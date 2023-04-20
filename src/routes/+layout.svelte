647560a7316dd7a0c322ccaa39a9b8915fe29fd7
Lance Armah-Abraham <lanarmah@gmail.com>
2023-04-20 18:06:07 +0000

    feat: inject vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
+	import { inject } from '@vercel/analytics';
 	import GlobalNavigationSpinner from '$components/UI/spinners/GlobalNavigationSpinner.svelte';
 	import '../app.css';
+
+	inject();
 </script>
 
 <div class="relative">
